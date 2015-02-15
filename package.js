a6076b31f4b737a1857c1e625544a07cd8c2b90d
stlouisweb <stlouisweb@gmail.com>
2015-02-15 04:37:56 +0000

    adds version numbers to dependencies.

--- package.js
+++ package.js
@@ -2,13 +2,13 @@ Package.describe({
   name: 'jplack:uploadmanager',
   version: '0.0.1',
   summary: 'adds upload manager for tomi:upload-server.',
-  git: 'https://github.com/stlouisweb/uploadmanager.git',
+  git: '',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-  api.use(['mongo', 'templating', 'tomi:upload-server', 'tomi:upload-jquery'], ['client', 'server']);
+  api.use(['mongo', 'templating', 'tomi:upload-server@1.1.1', 'tomi:upload-jquery@2.0.0'], ['client', 'server']);
   api.imply('templating')
   api.add_files(['uploadmanager.html', 'uploadmanager.js'], ['client', 'server']);
 });
