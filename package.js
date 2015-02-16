7995c2e05f4a5d11f5270fa6cbb4d43c8d8dd902
stlouisweb <stlouisweb@gmail.com>
2015-02-16 21:53:30 +0000

    version bump.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jplack:uploadmanager',
-  version: '0.0.3',
+  version: '0.0.4',
   summary: 'adds upload manager for tomi:upload-server.',
   git: 'https://github.com/stlouisweb/uploadmanager',
   documentation: 'README.md'
